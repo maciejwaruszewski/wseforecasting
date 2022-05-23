63e4f0f7ff53def5fc8efec55331ee31067d35b5
Maciej <90342007+maciejwaruszewski@users.noreply.github.com>
2022-05-23 22:13:44 +0200

    1.0.3 Datasets prepared, Pearsons correlation calculated

--- main.py
+++ main.py
@@ -1,36 +1,46 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.stats import pearsonr
 
 df1 = pd.read_csv('cpiypl_m_d.csv')
-df2 = pd.read_csv('usdpln_d.csv')
-df3 = pd.read_csv('wig20_d.csv')
+df2 = pd.read_csv('usdpln_m.csv')
+df3 = pd.read_csv('wig20_m.csv')
+df4 = df2['Data']
 
-# cpi plot
-# i = df1['Data']
-# j = df1['Zamkniecie']
+df1['Month'] = pd.DatetimeIndex(df1['Data']).month
+df1['Year'] = pd.DatetimeIndex(df1['Data']).year
+df1['Period'] = pd.to_datetime(df1[['Year', 'Month']].assign(DAY=1))
 
-# plt.plot(i, j)
-# plt.xlabel('Date')
-# plt.ylabel('Inflation')
-# plt.title('CPI Y/Y')
-# plt.show()
 
-# usdpln plot
-# k = df2['Data']
-# m = df2['Zamkniecie']
+df2['Month'] = pd.DatetimeIndex(df2['Data']).month
+df2['Year'] = pd.DatetimeIndex(df2['Data']).year
+df2['Period'] = pd.to_datetime(df2[['Year', 'Month']].assign(DAY=1))
 
-# plt.plot(k, m)
-# plt.xlabel('Date')
-# plt.ylabel('Currency rate')
-# plt.title('USDPLN D/D')
-# plt.show()
+df3['Month'] = pd.DatetimeIndex(df3['Data']).month
+df3['Year'] = pd.DatetimeIndex(df3['Data']).year
+df3['Period'] = pd.to_datetime(df3[['Year', 'Month']].assign(DAY=1))
 
-# wig20 plot
-# n = df2['Data']
-# o = df2['Zamkniecie']
+corr1, _ = pearsonr(df1['Zamkniecie'], df3['Zamkniecie'])
+print('Pearsons correlation: %.3f' % corr1)
 
-# plt.plot(n, o)
-# plt.xlabel('Date')
-# plt.ylabel('Pts')
-# plt.title('WIG20 D/D')
-# plt.show()
+corr2, _ = pearsonr(df1['Zamkniecie'], df2['Zamkniecie'])
+print('Pearsons correlation: %.3f' % corr2)
+
+corr3, _ = pearsonr(df3['Zamkniecie'], df2['Zamkniecie'])
+print('Pearsons correlation: %.3f' % corr3)
+
+fix, ax1 = plt.subplots()
+
+date = df1['Period']
+cpi = df1['Zamkniecie']
+usd = df2['Zamkniecie']
+wig = df3['Zamkniecie']
+
+ax1.plot(date,cpi, color='blue')
+
+ax2 = ax1.twinx()
+ax2.plot(date,usd, color='red')
+
+ax3 = ax1.twinx()
+ax3.plot(date,wig, color='green')
+plt.show()
